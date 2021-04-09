96d83c9e5a1deb94b0c4392b699ee7c3ce850b40
Felix Geisendörfer <felix@datadoghq.com>
2021-04-09 12:09:04 +0200

    Hide fuzz behind build tag

--- fuzz.go
+++ fuzz.go
@@ -3,13 +3,14 @@
 // This product includes software developed at Datadog (https://www.datadoghq.com/).
 // Copyright 2016 Datadog, Inc.
 
+//+build fuzz
+
 package gostackparse
 
 import "bytes"
 
 // Fuzz implements fuzzing using https://github.com/dvyukov/go-fuzz. See
-// TestFuzzCorupus for generating an initial test corpus. This API is not part
-// of the semver compatability of this module, do not reference it.
+// TestFuzzCorupus for generating an initial test corpus.
 func Fuzz(data []byte) int {
 	goroutines, _ := Parse(bytes.NewReader(data))
 	if len(goroutines) > 0 {
